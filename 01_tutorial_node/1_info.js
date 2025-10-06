a2d99fa5b5fc91c26aa4a9b7658169ae582e8752
Đinh Văn Khôi <dinhvankhoi.vn@outlook.com>
2025-10-06 13:28:52 +0700

    mẫu điều kiện

---
+++ 01_tutorial_node/1_info.js
@@ -0,0 +1,8 @@
+let count = 0;
+if (count < 10) {
+  console.log("count is less than 10");
+} else if (count > 10) {
+  console.log("count is greater than 10");
+} else {
+  console.log("count is equal to 10");
+}
