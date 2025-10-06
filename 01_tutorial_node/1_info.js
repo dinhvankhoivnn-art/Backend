9bb5cd6c5d13300ca7cbe372faaa56422cdba4c7
Đinh Văn Khôi <dinhvankhoi.vn@outlook.com>
2025-10-06 13:32:42 +0700

    giới thiệu thêm vài tính năng

--- 01_tutorial_node/1_info.js
+++ 01_tutorial_node/1_info.js
@@ -6,3 +6,20 @@ if (count < 10) {
 } else {
   console.log("count is equal to 10");
 }
+
+// ! global:
+
+// __dirname ==> thư mục hiện tại
+// __filename ==> file hiện tại
+// require => import file
+// module ==> lấy module
+// process => lấy biến môi trường .env
+
+console.log(__filename);
+setTimeout(function () {
+  console.log(__dirname);
+  console.log(__filename);
+  console.log(require);
+  console.log(module);
+  console.log(process);
+}, 1000);
