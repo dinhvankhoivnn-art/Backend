190c44ef5cb76f85f377b1a1ec888427ab57d615
Đinh Văn Khôi <dinhvankhoi.vn@outlook.com>
2025-10-25 16:45:12 +0700

    cấu hình server có toàn quyền kiểm soát server thay vì chạy liên tục

--- devcamper.io/app.js
+++ devcamper.io/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 
 const bootcamps = require("./routes/bootcamps");
 const { logger } = require("./middleware/logger");
+const { connectDB } = require("./db/connectDB");
 
 dotenv.config({ path: ".env" });
 const app = express();
@@ -15,9 +16,18 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 app.use(logger);
+// ! kết nối db
+connectDB();
 const PORT = process.env.PORT || 5000;
 // ! cấu hình middleware
 // ! use Router
 app.use("/v1/bootcamps", bootcamps);
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}`)
+);
+// ! handle unhandled promise rejection
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  server.close(() => process.exit(1));
+});
