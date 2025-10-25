71f4d327a8c9ecf787327f579fe470a2dafe5452
Đinh Văn Khôi <dinhvankhoi.vn@outlook.com>
2025-10-25 15:52:33 +0700

    tách router thành controllers

--- devcamper.io/routes/bootcamps.js
+++ devcamper.io/routes/bootcamps.js
@@ -1,39 +1,19 @@
+const {
+  getAllBootcamps,
+  createBootcamp,
+  getBootcampForID,
+  updateBootcamp,
+  deleteBootcamp,
+} = require("../controllers/bootcamps");
+
 const router = require("express").Router();
-// ! get all bootcamps
-router.get("/", (req, res) => {
-  res.status(200).json({
-    status: true,
-    message: `get all bootcamps successfully`,
-  });
-});
-// ! create bootcamp
-router.post("/", (req, res) => {
-  res.status(200).json({
-    status: true,
-    message: `create bootcamp successfully`,
-  });
-});
-// ! get bootcamp for ID
-router.get("/:id", (req, res) => {
-  res.status(200).json({
-    status: true,
-    message: `get bootcamp with id ${req.params.id} successfully`,
-  });
-});
+// ! lấy tất cả bootcamp - thêm bootcamp
+router.route("/").get(getAllBootcamps).post(createBootcamp);
 
-// ! update bootcamp for ID
-router.put("/:id", (req, res) => {
-  res.status(200).json({
-    status: true,
-    message: `update bootcamp with id ${req.params.id} successfully`,
-  });
-});
+router
+  .route("/:id")
+  .get(getBootcampForID)
+  .get(updateBootcamp)
+  .delete(deleteBootcamp);
 
-// ! delete bootcamp for ID
-router.delete("/:id", (req, res) => {
-  res.status(200).json({
-    status: true,
-    message: `delete bootcamp with id ${req.params.id} successfully`,
-  });
-});
 module.exports = router;
