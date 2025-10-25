72193db0a04ee0926242b6eabc8732a0f2392133
Đinh Văn Khôi <dinhvankhoi.vn@outlook.com>
2025-10-25 16:13:41 +0700

    cấu hình thêm morgan middleware

--- devcamper.io/app.js
+++ devcamper.io/app.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+
 const bootcamps = require("./routes/bootcamps");
+const { logger } = require("./middleware/logger");
 
 dotenv.config({ path: ".env" });
 const app = express();
 // ! middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// ! kiểm tra điều kiện
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(logger);
 const PORT = process.env.PORT || 5000;
+// ! cấu hình middleware
 // ! use Router
 app.use("/v1/bootcamps", bootcamps);
 
