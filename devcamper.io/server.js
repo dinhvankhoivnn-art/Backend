20d01adf4ff0af8280e9f14594a24dd4d33ce8da
Đinh Văn Khôi <dinhvankhoi.vn@outlook.com>
2025-10-25 15:11:43 +0700

    cấu hình router

--- devcamper.io/server.js
+++ devcamper.io/server.js
@@ -5,11 +5,38 @@ env.config({ path: "./config/config.env" });
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
+// ! cấu hình app
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//  ACRUD
+app.get("/v1/bootcamps", (req, res) => {
   res.status(200).send({
     status: true,
-    message: "Show all  bootcamps",
+    message: `Get all bootcamps success`,
+  });
+});
+app.post("/v1/bootcamps", (req, res) => {
+  res.status(200).send({
+    status: true,
+    message: `Create bootcamps success`,
+  });
+});
+app.get("/v1/bootcamps/:id", (req, res) => {
+  res.status(200).send({
+    status: true,
+    message: `Get bootcamps for ID ${req.params.id} success`,
+  });
+});
+app.put("/v1/bootcamps/:id", (req, res) => {
+  res.status(200).send({
+    status: true,
+    message: `Update bootcamps for ID ${req.params.id} success`,
+  });
+});
+app.delete("/v1/bootcamps/:id", (req, res) => {
+  res.status(200).send({
+    status: true,
+    message: `Delete bootcamps for ID ${req.params.id} success`,
   });
 });
 app.listen(PORT, () => {
